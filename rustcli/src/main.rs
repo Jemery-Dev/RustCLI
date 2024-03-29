3b7a85a4a3392924d8cb3323b56f753157025d0d
Jérémy Girard <oqaislenning@gmail.com>
2024-03-29 11:58:31 +0100

    Ajout ls

--- rustcli/src/main.rs
+++ rustcli/src/main.rs
@@ -1,25 +1,52 @@
-use crate::utils::echo_function;
-use std::io;
+    use crate::utils::{echo_function, ls_function};
+    use std::io;
+    use std::io::Error;
 
-mod utils;
+    mod utils;
 
-fn main() {
-    loop {
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Impossible de lire l'input");
+    fn main() {
+        loop {
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Impossible de lire l'input");
 
-        let list_inputs: Vec<String> = input.trim().split(' ').map(String::from).collect();
+            let list_inputs: Vec<String> = input.trim().split(' ').map(String::from).collect();
 
-        if !list_inputs.is_empty() {
-            match list_inputs[0].as_str() {
-                "echo" => {echo_function(list_inputs[1].as_str());},
-                "pipi" => {println!("pipi commande ?");},
-                _ => {println!("Commande {} inconnu au bataillon", list_inputs[0].as_str());}
+            if !list_inputs.is_empty() {
+                match list_inputs[0].as_str() {
+                    "echo" => {
+                        echo_function(list_inputs[1].as_str());
+                    },
+
+                    "pipi" => {
+                        println!("pipi commande ?");
+                    },
+
+                    "ls" => {
+                        let ls_param = if list_inputs.len() > 1 {
+                            &list_inputs[1]
+                        } else {
+                            "../fichier_test/"
+                        };
+                        match ls_function(ls_param){
+                            Ok(entries) => {
+                                for entry in entries{
+                                    println!("{}", entry);
+                                }
+                            }
+                            Err(_) => {
+                                println!("Erreur lors de l'affichage du répertoire");
+                            }
+                        }
+                    }
+
+                    _ => {
+                        println!("Commande {} inconnu au bataillon", list_inputs[0].as_str());
+                    }
+                }
+            } else {
+                println!("{}", "");
             }
-        } else {
-            println!("{}", "");
         }
     }
-}
--- rustcli/src/utils.rs
+++ rustcli/src/utils.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::Path;
+
 pub fn echo_function(input: &str){
     if input.is_empty() {
         String::new();
@@ -5,3 +8,36 @@ pub fn echo_function(input: &str){
         println!("{}", input);
     }
 }
+
+pub fn ls_function(input: &str) -> Result<Vec<String>, std::io::Error>{
+        let mut entries_list = Vec::new();
+        let dir = Path::new(input);
+        if dir.is_dir() {
+            if let Ok(entries) = fs::read_dir(dir) {
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        if let Ok(nom_fic) = entry.file_name().into_string() {
+                            entries_list.push(nom_fic);
+                        } else {
+                            entries_list.push("Fichier corrompu".to_string());
+                        }
+                    }
+                }
+            }
+            Ok(entries_list)
+        } else {
+            Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Not a directory"))
+        }
+    }
+
+pub fn cat_function(file_names: Vec<String>) -> Result<Vec<String>, std::io::Error> {
+    let mut files_contents = Vec::new();
+        if (file_names) {
+
+
+            Ok(files_contents)
+        } else{
+        Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "File doesn't exist"))
+    }
+}
+
