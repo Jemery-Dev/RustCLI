4451fb92aaf1a220829f261bc14fb3aeb6a92646
Jérémy Girard <oqaislenning@gmail.com>
2024-04-03 15:04:09 +0200

    Ajout de commentaires

--- rustcli/src/main.rs
+++ rustcli/src/main.rs
@@ -11,10 +11,12 @@
                 .read_line(&mut input)
                 .expect("Impossible de lire l'input");
 
+            // On récupère chaque input avec un split (' ')
             let list_inputs: Vec<String> = input.trim().split(' ').map(String::from).collect();
 
+            // Si jamais y a une commande
             if !list_inputs.is_empty() {
-                match list_inputs[0].as_str() {
+                match list_inputs[0].as_str() { // Match de la commande
                     "echo" => {
                         echo_function(list_inputs[1].as_str());
                     },
@@ -23,43 +25,44 @@
                         println!("pipi commande ?");
                     },
 
-                    "ls" => {
+                    "ls" => { // La commande ls affiche tout les dossiers d'un dir
                         let ls_param = if list_inputs.len() > 1 {
-                            &list_inputs[1]
+                            &list_inputs[1] // On check s'il y a un param
                         } else {
-                            "./"
+                            "./" // Sinon on prend le dir courant
                         };
-                        match ls_function(ls_param){
-                            Ok(entries) => {
-                                for entry in entries{
-                                    println!("{}", entry);
+                        match ls_function(ls_param){ // On récupère les informations de ls_function
+                            Ok(entries) => { // Si jamais ls_function renvoie Ok
+                                for entry in entries{ // On passe à travers tout les fichiers
+                                    println!("{}", entry); // On affiche le file
                                 }
                             }
-                            Err(_) => {
-                                println!("Erreur lors de l'affichage du répertoire");
+                            Err(_) => { // Si jamais ça renvoie une erreur
+                                println!("Erreur lors de l'affichage du répertoire"); // On le précise
                             }
                         }
                     }
 
-                    "cat" => {
-                        if list_inputs.len() > 1 {
+                    "cat" => { // Cat concatène le contenu des fichiers en param
+                        if list_inputs.len() > 1 { // Si jamais y a au moins un param
+                            // Pour chaque param, on le Vec cat param
                             let cat_param: Vec<String> = list_inputs[1..].iter().map(|s| s.to_string()).collect();
-                            match cat_function(cat_param) {
-                                Ok(contents) => {
+                            match cat_function(cat_param) { // On passe le vec dans la fonction
+                                Ok(contents) => { // Si la fonction est réussi
                                     for content in contents {
-                                        println!("{}", content);
+                                        println!("{}", content); // On écrit le contenun
                                     }
                                 }
-                                Err(_) => {
-                                    println!("Erreur lors de la lecture du fichier");
+                                Err(_) => { // Sinon avec une erreur
+                                    println!("Erreur lors de la lecture du fichier"); // On le précise
                                 }
                             }
-                        } else {
+                        } else { // Si pas de param, on précise aussi
                             println!("Aucun fichier spécifié pour la commande cat");
                         }
                     }
 
-                    _ => {
+                    _ => { // Si aucune des commandes ne correspond
                         println!("Commande {} inconnu au bataillon", list_inputs[0].as_str());
                     }
                 }
